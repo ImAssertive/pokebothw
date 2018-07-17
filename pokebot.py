e731a27fd48ade9974388a02a1b7d58d3f4c0e8c
ImAssertive <imassertive@gmail.com>
2018-07-17 15:47:36 +0100

    developing/initial commit

---
+++ pokebot.py
@@ -0,0 +1,83 @@
+import discord, asyncio, sys, traceback, checks, asyncpg, useful, credentialsFile
+from discord.ext import commands
+
+def getPrefix(bot, message):
+    prefixes = ["pb!", "p!","pokebot!"]
+    return commands.when_mentioned_or(*prefixes)(bot, message)
+
+async def run():
+    description = "Pokemon Bot High Wycombe! pb!help for commands."
+    credentials = credentialsFile.getCredentials()
+    db = await asyncpg.create_pool(**credentials)
+    await db.execute('''CREATE TABLE IF NOT EXISTS Users(userID bigint PRIMARY KEY,
+    banned boolean DEFAULT false);
+    
+    CREATE TABLE IF NOT EXISTS Pokestops(stopID bigint PRIMARY KEY,
+    name text,
+    screenshoturl text,
+    mapurl text,
+    imageurl text,
+    coord text,
+    type text);
+    
+    CREATE TABLE IF NOT EXISTS Pokemon(pokedex bigint PRIMARY KEY,
+    name text,
+    imageurl text,
+    description text,
+    screenshoturl text);''')
+    bot = Bot(description=description, db=db)
+    initial_extensions = ['pokemon', 'misc']
+    if __name__ == '__main__':
+        for extension in initial_extensions:
+            try:
+                bot.load_extension(extension)
+            except Exception as e:
+                print('Failed to load extension ' + extension, file=sys.stderr)
+                traceback.print_exc()
+
+    try:
+        await bot.start(credentialsFile.getToken())
+    except KeyboardInterrupt:
+        await db.close()
+        await bot.logout()
+
+class Bot(commands.Bot):
+    def __init__(self, **kwargs):
+        super().__init__(
+            description=kwargs.pop("description"),
+            command_prefix=getPrefix
+        )
+
+        self.pubquizAnswers = []
+        self.db = kwargs.pop("db")
+        self.currentColour = -1
+        self.outcomes = ["It is certain", "It is decidedly so", "Without a doubt", "Yes - definitely",
+                    "You may rely on it",
+                    "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes",
+                    "Reply hazy, try again", "Ask again later", "Better not tell you now",
+                    "Cannot predict now", "Concentrate and ask again", "Don't count on it",
+                    "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+
+    async def on_ready(self):
+        print("Username: {0}\nID: {0.id}".format(self.user))
+        game = discord.Game("Pokemon Go!")
+        await self.change_presence(status=discord.Status.online, activity=game)
+
+    def getcolour(self):
+        colours = ["5C6BC0", "AB47BC", "EF5350", "FFA726", "FFEE58", "66BB6A", "5BCEFA", "F5A9B8", "FFFFFF", "F5A9B8", "5BCEFA"]
+        self.currentColour += 1
+        if self.currentColour ==  len(colours):
+            self.currentColour = 0
+        return discord.Colour(int(colours[self.currentColour], 16))
+
+    def conchcolour(self, number):
+        if number < 10 and number > -1:
+            return discord.Colour(int("00FF00", 16))
+        elif number > 9 and number < 15:
+            return discord.Colour(int("FFFF00", 16))
+        else:
+            return discord.Colour(int("FF0000", 16))
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run())
---
+++ pokemon.py
@@ -0,0 +1,49 @@
+import discord, asyncio, sys, traceback, checks, inflect, useful, random
+from discord.ext import commands
+
+class pokeCog:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def info(self, ctx, *, stopname):
+        query = "SELECT * FROM Pokestops WHERE name = $1"
+        result = await ctx.bot.db.fetchrow(query, stopname.lower())
+        if not result:
+            await ctx.channel.send(":no_entry: | No Pokestop with name:** "+stopname+"** found.")
+        else:
+            await ctx.channel.send("wew")
+
+    @commands.command()
+    @checks.justme()
+    async def addstop(self, ctx):
+        stoptype = "wew"
+        await ctx.channel.send(":rotating_light: | Please enter the name of the pokestop.")
+        def check(msg):
+            options = ["gym", "pokestop", "stop"]
+            return ctx.channel.id == msg.channel.id and msg.author.id == ctx.author.id and msg.content.lower() in options
+        try:
+            msg = await self.bot.wait_for('message', check=check, timeout=60.0)
+        except asyncio.TimeoutError:
+            await ctx.channel.send(":no_entry: | **" + ctx.author.display_name + "** The command window has closed due to inactivity. Please use the addstop command again to restart the proccess.")
+        else:
+            if msg.content.lower == "gym":
+                stoptype = "gym"
+            elif msg.content == "pokestop" or msg.content == "stop":
+                stoptype = "pokestop"
+        if stoptype != "wew":
+            print("do the rest of the program")
+        else:
+            print("timeout i think?")
+
+
+
+        # stoptextlist = [[":rotating_light: | Please enter the name of the pokestop.", "name", ""],
+        #                 [":rotating_light: | Please enter the url of the screenshot of the pokestop.", "screenshoturl", ""],
+        #                 [":rotating_light: | Please enter the url of the map location of the pokestop.", "mapurl", ""],
+        #                 [":rotating_light: | Please enter the url of the photo of the pokestop.", "imageurl", ""],
+        #                 [":rotating_light: | Please enter the map coordanites of the pokestop.", "coord", ""]]
+
+
+def setup(bot):
+    bot.add_cog(pokeCog(bot))
